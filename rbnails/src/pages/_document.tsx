64b57b9565d5080fb0081f209f7a1503a7b07d80
liniker <linikers@hotmail.com>
2023-05-09 15:28:50 -0300

    versão com meta

--- rbnails/src/pages/_document.tsx
+++ rbnails/src/pages/_document.tsx
@@ -9,6 +9,25 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         />
+
+        <title>Rafa Bach | Manicure em Maringá-PR</title>
+        <meta
+          name="description"
+          content="Rafa Bach é uma manicure profissional em Maringá-PR. Oferecemos serviços de manicure, pedicure e design de unhas. Agende seu horário agora!"
+        />
+        <meta
+          property="og:title"
+          content="Rafa Bach | Manicure em Maringá-PR"
+        />
+        <meta
+          property="og:description"
+          content="Rafa Bach é uma manicure profissional em Maringá-PR. Oferecemos serviços de manicure, pedicure e design de unhas. Agende seu horário agora!"
+        />
+        <meta property="og:url" content="https://rafabach.com.br/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:region" content="Maringá-PR" />
+        <meta property="og:service" content="Manicure" />
       </Head>
       <body>
         <Main />
--- rbnails/src/pages/index.tsx
+++ rbnails/src/pages/index.tsx
@@ -36,27 +36,6 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Rafa Bach | Manicure em Maringá-PR</title>
-        <meta
-          name="description"
-          content="Rafa Bach é uma manicure profissional em Maringá-PR. Oferecemos serviços de manicure, pedicure e design de unhas. Agende seu horário agora!"
-        />
-        <meta
-          property="og:title"
-          content="Rafa Bach | Manicure em Maringá-PR"
-        />
-        <meta
-          property="og:description"
-          content="Rafa Bach é uma manicure profissional em Maringá-PR. Oferecemos serviços de manicure, pedicure e design de unhas. Agende seu horário agora!"
-        />
-        <meta property="og:url" content="https://rafabach.com.br/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="pt_BR" />
-        <meta property="og:region" content="Maringá-PR" />
-        <meta property="og:service" content="Manicure" />
-      </Head>
-
       <Container>
         <header className="custom-header">
           <figure className="logo">
