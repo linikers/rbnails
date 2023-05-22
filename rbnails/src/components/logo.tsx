684a764580230b4ca6a75385a8737351e4ead337
liniker <linikers@hotmail.com>
2023-05-22 13:16:43 -0300

    att link logo

--- rbnails/src/components/logo.tsx
+++ rbnails/src/components/logo.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 export default function Logo() {
   return (
     <div>
-      <figure className="logo">
-        <p>Rafa Bach</p>
-        <span> Nail&apos;s</span>
-      </figure>
+      <Link href="/">
+        <figure className="logo">
+          <p>Rafa Bach</p>
+          <span> Nail&apos;s</span>
+        </figure>
+      </Link>
     </div>
   );
 }
