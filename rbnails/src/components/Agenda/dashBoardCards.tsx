91685e01bb7fe09d60952e758323099eaa3121c0
linikers <linikers@hotmail.com>
2025-09-03 17:14:42 -0300

    cards dash com mui

---
+++ rbnails/src/components/Agenda/dashBoardCards.tsx
@@ -0,0 +1,39 @@
+import { calcularTotalConfirmado, getAgendamentoByUser } from "@/lib/agendamentoStorage";
+import { Card, CardContent, Grid, Typography } from "@mui/material";
+// import Grid from "@mui/material/Grid";
+
+
+
+interface Props {
+    userId: string;
+}
+export default function DashboardCards({ userId }: Props) {
+
+    const agendamentos = getAgendamentoByUser(userId);
+    const totalConfirmados = calcularTotalConfirmado(userId);
+    const qtdAtendimentos = agendamentos.length;
+    const qtdConfirmados = agendamentos.filter(a => a.confirmado).length;
+    const clientesUnicos = new Set(agendamentos.map(a=> a.descricao)).size;
+
+    const items = [
+        { label: "Atendimentos", value: qtdAtendimentos },
+        { label: "Confirmados", value: qtdConfirmados },
+        { label: "Clientes únicos", value: clientesUnicos },
+        { label: "Total COnfirmado", value: `R$ ${totalConfirmados.toFixed(2)}` },
+    ];
+
+    return (
+        <Grid container>
+            {items.map((item: any, idx: number) => (
+                <Grid item xs={12} sm={6} md={3} key={idx}>
+                    <Card sx={{ borderRadius: 2, boxShadow: 3}}>
+                        <CardContent>
+                            <Typography variant="h6" color="primary">{item.label}</Typography>
+                            <Typography variant="h4">{item.value}</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            ))}
+        </Grid>
+    )
+}
