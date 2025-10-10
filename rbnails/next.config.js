6eaff1aa33816fdcdc6d95c81d4d9707849a1aa5
linikers <linikers@hotmail.com>
2025-10-10 16:07:46 -0300

    .

--- rbnails/next.config.js
+++ rbnails/next.config.js
@@ -1,3 +1,94 @@
+// const path = require('path');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   webpack: (config, { isServer }) => {
+//     // Configura o Sass
+//     config.module.rules.push({
+//       test: /\.scss$/,
+//       use: [
+//         "style-loader",
+//         "css-loader",
+//         {
+//           loader: "sass-loader",
+//           options: {
+//             sassOptions: {
+//               includePaths: [path.join(__dirname, "styles")],
+//             },
+//           },
+//         },
+//       ],
+//     });
+  
+//     // Configura o CSS
+//     config.module.rules.push({
+//       test: /\.css$/,
+//       use: [
+//         {
+//           loader: "style-loader",
+//         },
+//         {
+//           loader: "css-loader",
+//           options: {
+//             esModule: false,
+//             importLoaders: 1,
+//           },
+//         },
+//         {
+//           loader: "postcss-loader",
+//           options: {
+//             postcssOptions: {
+//               plugins: [
+//                 [
+//                   "autoprefixer",
+//                   {
+//                     // Configura as regras do autoprefixer
+//                     overrideBrowserslist: ["last 2 version", ">1%", "ie >= 11"],
+//                   },
+//                 ],
+//               ],
+//             },
+//           },
+//         },
+//       ],
+//     });
+  
+//     // Configura o File Loader para lidar com arquivos de fonte
+//     config.module.rules.push({
+//       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+//       use: [
+//         {
+//           loader: "file-loader",
+//           options: {
+//             name: "[name].[ext]",
+//             outputPath: "static/fonts/",
+//             publicPath: "/_next/static/fonts/",
+//           },
+//         },
+//       ],
+//     });
+  
+//     // Redireciona a pasta static
+//     if (!isServer) {
+//       config.plugins.push(
+//         new CopyWebpackPlugin({
+//           patterns: [
+//             {
+//               from: path.join(__dirname, "static"),
+//               to: path.join(__dirname, "public", "_next", "static"),
+//             },
+//           ],
+//         })
+//       );
+//     }
+  
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+
 webpack: (config, { isServer }) => {
   // Configura o Sass
   config.module.rules.push({
