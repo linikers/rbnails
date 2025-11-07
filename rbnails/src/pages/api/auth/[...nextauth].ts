ea1f2ad8f9f14a98a290581e8e61a4f8d99ce5c5
linikers <linikers@hotmail.com>
2025-11-07 11:34:29 -0300

    .

--- rbnails/src/pages/api/auth/[...nextauth].ts
+++ rbnails/src/pages/api/auth/[...nextauth].ts
@@ -13,34 +13,8 @@ export default NextAuth({
         user: { label: "Username", type: "text", placeholder: "jsmith" },
         pass: { label: "Password", type: "password" }
       },
-      async authorize(credentials, req) {
-        // console.log('chave', process.env.NEXTAUTH_SECRET);
-        // console.log('url', process.env.NEXTAUTH_URL);
-        // // Verifique se as credenciais foram fornecidas
-        // console.log('req', req);
-        // console.log('crede', credentials);
-        // if (!credentials) {
-        //   return null; // Autenticação falhou se não houver credenciais
-        // }
+      async authorize(credentials) {
 
-
-        // const users = [
-        //   { id: "1", name: "admin", email: "admin@mail.com", pass: "admin123", user: "admin" }, // Adicione o campo username aqui
-        //   { id: "2", name: "rafaela", email: "admin@mail.com", pass: "admin123", user: "rafaela" },
-        //   { id: "3", name: "jessica", email: "admin@mail.com", pass: "admin123", user: "jessica" },
-        //   { id: "4", name: "mari", email: "admin@mail.com", pass: "admin123", user: "mari" },
-        //   { id: "5", name: "sofia", email: "admin@mail.com", pass: "admin123", user: "luzia" },
-        // ]
-
-        // const user = users.find(u => u.user === credentials.user && u.pass === credentials.pass)
-
-        // if (user) {
-        //   // Qualquer objeto retornado aqui será salvo no token JWT e na sessão
-        //   return { id: user.id, name: user.name, email: user.email }
-        // } else {
-        //   // Se você retornar null ou false, a autenticação falhará
-        //   return null
-        // }
         if (!credentials?.user || !credentials?.pass) {
           throw new Error("Credenciais inválidas.");
         }
